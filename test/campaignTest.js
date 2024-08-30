7b3a0348f9f32c226e6f3136bc3bff69a78cdefb
Pranjal Pathak <pranjalpathak010@gmail.com>
2024-08-30 11:27:01 +0530

    new changes

---
+++ test/campaignTest.js
@@ -0,0 +1,11 @@
+
+
+const {Builder, Browser, By, value}= require('selenium-webdriver')
+const assert =  require('assert')
+const mocha = require('mocha')
+require('dotenv').config()
+const {create_RO} =require('../tests/ro')
+
+describe('RO and Campaign end to end',()=>{
+
+})
