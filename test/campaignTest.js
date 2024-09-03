de3578dde6a6c841260bebbbdc8eacfdd33f2231
Pranjal Pathak <pranjalpathak010@gmail.com>
2024-09-03 11:08:05 +0530

    execute ro creation then proceed

--- test/campaignTest.js
+++ test/campaignTest.js
@@ -1,97 +1,71 @@
-
-
-const { Builder, Browser, By, value } = require('selenium-webdriver')
+const { Builder, Browser, By } = require('selenium-webdriver');
 const assert = require('assert');
 const mocha = require('mocha');
 require('dotenv').config();
 const { create_RO } = require('../tests/ro');
 
-let ro_no
-async function getro() {
-    ro_no = await create_RO();
-    console.log("ro_no", ro_no);
-
-    return ro_no
-}
-
-describe('RO and Campaign end to end', () => {
-    let driver
+describe('RO and Campaign end-to-end', () => {
+    let driver;
     let website_url = 'https://staging-connect.paytunes.in/';
+    let ro_id;
 
     let id_username = process.env.username_staging;
     let id_password = process.env.password_staging;
 
     before(async function () {
-
         driver = await new Builder().forBrowser(Browser.CHROME).build();
         console.log("Driver accessed successfully");
 
+        // Fetching RO creation before other tests
+        let ro_no;
+        async function getro() {
+            ro_no = await create_RO();
+            console.log("RO created:", ro_no);
+            return ro_no;
+        }
+
+        ro_no = await getro(); // Await the asynchronous function
+        if (ro_no) {
+            ro_id = ro_no.RO_no_search_part;
+            console.log("RO number fetched successfully:", ro_id);
+        } else {
+            throw new Error("RO creation failed.");
+        }
     });
+
     after(async function () {
         await driver.quit();
         console.log("Closed WebDriver instance.");
     });
 
-
-
-    it('fetching RO creation', async function () {
-        //ro_creation
-
-
-        // await getro();
-        ro_no = await getro();
-        if (ro_no) {
-            console.log(ro_no.RO_no_search_part);
-        }
-        let ro_id = ro_no.RO_no_search_part
-
-    });
-
-    it('should load the STAGING paytunes website', async function () {
+    it('should load the STAGING PayTunes website', async function () {
         await driver.get(website_url);
         console.log("Website fetched and loaded successfully");
     });
-    //signin process
 
-    it('Enter the username ', async function () {
+    it('Enter the username', async function () {
         await driver.findElement(By.id("id_username")).clear();
         await driver.findElement(By.id('id_username')).sendKeys(id_username);
-        console.log("username entered correctly " + id_username);
+        console.log("Username entered correctly: " + id_username);
         await driver.sleep(1000);
-
     });
-
 
     it('Enter the password', async function () {
         await driver.findElement(By.id("id_password")).clear();
         await driver.findElement(By.id("id_password")).sendKeys(id_password);
-        console.log("the password entered is successfully " + id_password);
+        console.log("Password entered successfully: " + id_password);
         await driver.sleep(1000);
-
     });
 
-    //login to the site by clicking on signin button   
-    it('click on signin button to signin to the site', async function () {
-        await driver.findElement(By.xpath("//form[@id='login-form']//input[@type = 'submit' and @value='LOGIN']")).click();
-        console.log("signin button clicked successfully")
+    it('Click on sign-in button to sign in to the site', async function () {
+        await driver.findElement(By.xpath("//form[@id='login-form']//input[@type='submit' and @value='LOGIN']")).click();
+        console.log("Sign-in button clicked successfully");
         await driver.sleep(1000);
-
     });
 
-    //enter Agency by clicking on Agency    
-    it('Find,click and Enter on Agency ', async function () {
+    it('Find, click, and enter on Agency', async function () {
         await driver.findElement(By.xpath("//div[@style='width:100%']")).click();
-        // await driver.findElement(By.withTagName("h2 = Agency"));
-        console.log("clicked on agency successfully");
+        console.log("Clicked on Agency successfully");
         await driver.sleep(1000);
-
     });
-
-
-    it('', async function () {
-
-    });
-
-
-
 }).timeout(90000);
